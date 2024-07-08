c17c2617cab296588758546ddfd00355f4e335db
Rfluid <ruy.vieiraneto@gmail.com>
2024-07-08 12:00:14 -0300

    Add scheduler worker

--- src/integration/scheduler/main.integration.go
+++ src/integration/scheduler/main.integration.go
@@ -1,8 +1,16 @@
 package scheduler
 
-import "github.com/pterm/pterm"
+import (
+	"github.com/Rfluid/http-scheduler/src/config/env"
+	redis "github.com/Rfluid/http-scheduler/src/redis"
+	redis_scheduler "github.com/Rfluid/scheduler/src/redis"
+	"github.com/pterm/pterm"
+)
+
+var Worker *redis_scheduler.Worker
 
 func LoadIntegration() {
 	pterm.DefaultLogger.Info("Loading scheduler integration...")
+	Worker = redis_scheduler.Create(redis.Connection(), env.SchedulerListKey, env.SchedulerLockKey)
 	pterm.DefaultLogger.Info("Scheduler integration loaded")
 }
