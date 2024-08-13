b338416965d308f1e774622f25d94b96f6ab2f9b
Rfluid <ruy.vieiraneto@gmail.com>
2024-08-13 16:25:30 -0300

    Use Bearer auth at token middleware

--- src/auth/middleware/token.middleware.go
+++ src/auth/middleware/token.middleware.go
@@ -2,6 +2,7 @@ package auth_middleware
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Rfluid/http-scheduler/src/config/env"
 	"github.com/gofiber/fiber/v2"
@@ -16,7 +17,15 @@ func TokenMiddleware(c *fiber.Ctx) error {
 	if authHeader == "" {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": fmt.Sprintf("%s header is missing", env.AuthHeaderKey)})
 	}
-	if authHeader != env.AuthToken {
+
+	// Split the header to get the token
+	splitToken := strings.Split(authHeader, "Bearer ")
+	if len(splitToken) != 2 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": fmt.Sprintf("Invalid %s header length", env.AuthHeaderKey)})
+	}
+	tokenString := splitToken[1]
+
+	if tokenString != env.AuthToken {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Invalid token"})
 	}
 
